7b98bc771e39ff207545cc8ea161269d57299e5d
Muh Rahmatullah <muh.rahmatullah@kudo.co.id>
2018-09-02 17:52:42 +0700

    finish the projects

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lyricsView: UITextView!
+    @IBOutlet weak var nameFIeld: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameFIeld.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func reset(_ sender: Any) {
+        nameFIeld.text = ""
+        lyricsView.text = ""
+    }
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        let silly = lyricsFromName(lyricsTemplate: bananaFanaTemplate, fullName: nameFIeld.text!)
+        lyricsView.text = silly
+        
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: " \n ")
+
+func shortNameForName(name:String) -> String{
+    var name = name
+    name.remove(at: name.startIndex)
+    return name
+}
+func lyricsFromName(lyricsTemplate: String, fullName: String) -> String{
+    let shortName = shortNameForName(name: fullName)
+    let lyrics = lyricsTemplate
+        .replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    return lyrics
+}
